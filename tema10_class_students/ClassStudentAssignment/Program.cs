3ea5e7dd20c76ef6563fe743acc2954ef5aecffe
florinfmp <florinfmp@gmail.com>
2024-02-08 13:56:08 +0200

    tema 10 class students

---
+++ tema10_class_students/ClassStudentAssignment/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+
+namespace ClassStudentAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("%%%%%%%%%%%%%%%%%%%   class student assignment    %%%%%%%%%%%%%%%%%%%");
+            List<Student> myCollection = new List<Student>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Please insert student's name and age (18...99) with a white space between them: ");
+                string inputStr = Console.ReadLine();
+                string trimmedInputStr = inputStr.Trim();
+                string[] studentDetails = trimmedInputStr.Split(' ');
+                Student person = new Student(int.Parse(studentDetails[1]), studentDetails[0]);
+                myCollection.Add(person);
+            }
+
+            foreach (Student item in myCollection)
+            {
+                Console.WriteLine(item.Info);
+            }
+
+            foreach (Student item in myCollection)
+            {
+                Console.WriteLine($"Please insert student's {item.Name} mark or write 'pass' to move to next student: ");
+                string input = Console.ReadLine();
+                if (input == "pass")
+                {
+                    item.Mark = null;
+                }
+                else
+                {
+                    item.Mark = int.Parse(input);
+                }
+            }
+
+            int counter = 0;
+            int sum = 0;
+            foreach (Student item in myCollection)
+            {
+                if (item.Mark.HasValue)
+                {
+                    counter++;
+                    sum += (int)item.Mark;
+
+                }
+            }
+
+            Console.WriteLine($"Average mark of all students is {sum/counter}");
+
+            Console.WriteLine("%%%%%%%%%%%%%%%%%%%   End of class student assignment     %%%%%%%%%%%%%%%%%%%");
+        }
+    }
+}
---
+++ tema10_class_students/ClassStudentAssignment/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassStudentAssignment
+{
+    public class Student
+    {
+        private int _age;
+        private string _name;
+        public const int min_age = 18;
+        public const int max_age = 99;
+
+        public string Name
+        {
+            get { return _name; }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set { _age = (value >= min_age) ? ((value <= max_age) ? value: 0) : 0; }
+        }
+
+        public int? Mark { get; set; }
+
+        public string Info
+        {
+            get { return $"Person {_name} is {_age} years old."; }
+        }
+
+        public Student (int age, string name)
+        {
+            this._age = (age >= min_age) ? ((age <= max_age) ? age : 0) : 0;
+            this._name = name;
+        }
+    }
+}
