c934a47d2835f2fa02a49edfd60c9f19f5a12892
florinfmp <florinfmp@gmail.com>
2024-02-29 17:52:46 +0200

    tema 12 dog with exceptions

---
+++ tema12_dog_with_exceptions/DogWithExceptions/Dog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogWithExceptions
+{
+    public class Dog
+    {
+        //fields
+        private string _name;
+        private sbyte _age;
+
+        //properties
+        public string Name
+        {
+            get { return _name; }
+            set 
+            {
+                if (value.Length < 2)
+                {
+                    throw new System.ApplicationException("Length of characters for name is too short. Must beat least 2 characters.");
+                }
+                _name = value;
+            }
+        }
+
+        public sbyte Age
+        {
+            get { return _age; }
+            set 
+            {
+                if (value > 100)
+                {
+                    throw new System.ApplicationException("Age of dog is too big. Must be between 0 and max 100 years.");
+                }
+
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age of dog cannot be a negative number.");
+                }
+                _age = value; 
+            }
+        }
+
+        //Constructor
+        public Dog()
+        {
+            this._name = "test";
+            this._age = 0;
+        }
+    }
+}
---
+++ tema12_dog_with_exceptions/DogWithExceptions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml.Linq;
+
+namespace DogWithExceptions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("%%%%%%%%%%%%%%%%%%%   dog with exceptions assignment    %%%%%%%%%%%%%%%%%%%");
+
+            Console.WriteLine("Enter the name of the dog:");
+            string nameInput = Console.ReadLine();
+            Console.WriteLine("Enter the age of the dog:");
+            string ageInput = Console.ReadLine();
+
+            try
+            {
+                sbyte ageInputAsSByte = Convert.ToSByte(ageInput);
+                Dog myDog = new Dog { Name = nameInput, Age = ageInputAsSByte };
+                Console.WriteLine("No validation errors are present.");
+                Console.WriteLine($"Name for dog is: {myDog.Name}");
+                Console.WriteLine($"Age for dog is: {myDog.Age}");
+            }
+            catch (ApplicationException ex1)
+            {
+                Console.WriteLine("Error: " + ex1.Message);
+            }
+            catch (ArgumentException ex2)
+            {
+                Console.WriteLine("Error: " + ex2.Message);
+            }
+            catch
+            {
+                Console.WriteLine("Error: unexpected type. Contact admin.");
+                throw;
+            }
+
+            Console.WriteLine("%%%%%%%%%%%%%%%%%%%   End of dog with exceptions assignment    %%%%%%%%%%%%%%%%%%%");
+        }
+    }
+}
